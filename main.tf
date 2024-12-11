d5653234465a54e0871f864e82dd3a60dd7d4e79
Erwin Staal <erwin.staal@xebia.com>
2024-12-11 15:10:43 +0100

    Add initial Terraform configuration and provider setup

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "f0e483fc-9d2f-4a4b-8aee-887a398ff27e"
+}
+
+variable "location" {
+  description = "The location/region where the resource group will be created."
+  type        = string
+  default     = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-storageaccount"
+  location = var.location
+}
+
+module "storageaccount" {
+  source  = "staal-it/storageaccount/azure"
+  version = "0.1.3"
+
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "westeurope"
+}
